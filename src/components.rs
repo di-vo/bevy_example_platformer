85b40653add40b43578fc073d905557716b8922f
Jan <jan.reuter2909@gmail.com>
2024-04-03 11:59:29 +0200

    basic level setup

--- src/components.rs
+++ src/components.rs
@@ -2,3 +2,23 @@ use bevy::prelude::*;
 use bevy_ecs_ldtk::prelude::*;
 
 use bevy_rapier2d::prelude::*;
+
+#[derive(Default, Component)]
+struct Player;
+
+#[derive(Default, Bundle, LdtkEntity)]
+pub struct PlayerBundle {
+    player: Player,
+    #[sprite_sheet_bundle]
+    sprite_sheet_bundle: SpriteSheetBundle,
+}
+
+#[derive(Default, Component)]
+struct Goal;
+
+#[derive(Default, Bundle, LdtkEntity)]
+pub struct GoalBundle {
+    goal: Goal,
+    #[sprite_sheet_bundle]
+    sprite_sheet_bundle: SpriteSheetBundle,
+}
